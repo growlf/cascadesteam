b2b9429e805f1dd33dc86a5f5879a072c99572a2
NetYeti <growlfd@gmail.com>
2026-01-08 14:25:29 -0800

    fixed OTHER set error

--- .github/quartz/quartz.layout.ts
+++ .github/quartz/quartz.layout.ts
@@ -64,7 +64,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer({
       filterFn: (node) => {
         // set containing names of everything you want to filter out
-        const omit = new Set(["assets", "README")
+        const omit = new Set(["assets", "README"])
     
         // can also use node.slug or by anything on node.data
         // note that node.data is only present for files that exist on disk
