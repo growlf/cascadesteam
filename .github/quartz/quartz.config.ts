e3bb4ea832b159f3179508400a47aaed21ad7cd0
NetYeti <growlfd@gmail.com>
2026-01-08 17:46:58 -0800

    colors...

--- .github/quartz/quartz.config.ts
+++ .github/quartz/quartz.config.ts
@@ -29,7 +29,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#a66464ff",
+          light: "#ffffffff",
           lightgray: "#828282ff",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
